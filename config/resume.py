523a8cf9cfb8896ab6742c30a45ab6d56f7fb367
sahilkargutkar <sahilkargutkar.sk@gmail.com>
2026-03-10 10:13:10 +0530

    changes for sr software developer

---
+++ config/resume.py
@@ -0,0 +1,90 @@
+'''
+Author:     Sahil VIjay Kargutkar
+LinkedIn:   https://www.linkedin.com/in/sahilkar99/
+
+Copyright (C) 2024 Sahil VIjay Kargutkar
+
+License:    GNU Affero General Public License
+            https://www.gnu.org/licenses/agpl-3.0.en.html
+            
+GitHub:     
+
+Support me: 
+
+version:    26.01.20.5.08
+'''
+
+from personals import *
+import json
+
+###################################################### CONFIGURE YOUR RESUME HERE ######################################################
+
+
+# Give a path of your default resume to be uploaded. If file is not found, will continue using your previously uploaded resume in LinkedIn.
+default_resume_path = r"C:\Users\user\OneDrive\Desktop\Tailored Resume\SahilSDE2-FullStack.pdf"
+
+'''
+NOTE: Make sure the file extension (.pdf) matches your resume file.
+If the above path doesn't work, verify that:
+1. The file exists at this location
+2. The filename ends with .pdf or correct extension
+3. If needed, copy your resume to a simpler path like: "resume.pdf" in the project folder
+'''
+
+
+# resume_headline = json({
+#     "first_name": first_name,
+# })
+
+
+
+
+
+
+
+
+
+
+
+# # >>>>>>>>>>> RELATED SETTINGS <<<<<<<<<<<
+
+# ## Allow Manual Inputs
+# # Should the tool pause before every submit application during easy apply to let you check the information?
+# pause_before_submit = True         # True or False, Note: True or False are case-sensitive
+# '''
+# Note: Will be treated as False if `run_in_background = True`
+# '''
+
+# # Should the tool pause if it needs help in answering questions during easy apply?
+# # Note: If set as False will answer randomly...
+# pause_at_failed_question = True    # True or False, Note: True or False are case-sensitive
+# '''
+# Note: Will be treated as False if `run_in_background = True`
+# '''
+# ##
+
+# # Do you want to overwrite previous answers?
+# overwrite_previous_answers = False # True or False, Note: True or False are case-sensitive
+
+
+
+
+
+
+
+############################################################################################################
+'''
+THANK YOU for using my tool 😊! Wishing you the best in your job hunt 🙌🏻!
+
+Sharing is caring! If you found this tool helpful, please share it with your peers 🥺. Your support keeps this project alive.
+
+Support my work on <PATREON_LINK>. Together, we can help more job seekers.
+
+As an independent developer, I pour my heart and soul into creating tools like this, driven by the genuine desire to make a positive impact.
+
+Your support, whether through donations big or small or simply spreading the word, means the world to me and helps keep this project alive and thriving.
+
+Gratefully yours 🙏🏻,
+Sahil VIjay Kargutkar
+'''
+############################################################################################################
---
+++ modules/resumes/extractor.py
@@ -0,0 +1,21 @@
+'''
+Author:     Sahil VIjay Kargutkar
+LinkedIn:   https://www.linkedin.com/in/sahilkar99/
+
+Copyright (C) 2024 Sahil VIjay Kargutkar
+
+License:    GNU Affero General Public License
+            https://www.gnu.org/licenses/agpl-3.0.en.html
+            
+GitHub:     
+
+Support me: 
+
+'''
+
+
+from config.personals import *
+from config.questions import default_resume_path
+
+
+
